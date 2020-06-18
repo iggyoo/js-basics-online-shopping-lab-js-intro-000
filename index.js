ec51c478faffb0737eb4ab0e0d7596eaabef3b80
Scarlett Baez <s.baez1010@gmail.com>
2020-06-18 18:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(Bag) {
-var items = [{ itemName:"bananas", itemPrice: 17 }, { itemName:"pancake batter",itemPrice: 5 }, { itemName:"eggs", itemPrice: 49 }]
+var itemsObj = [{ itemName:"bananas", itemPrice: 17 }, { itemName:"pancake batter",itemPrice: 5 }, { itemName:"eggs", itemPrice: 49 }]
 }
 
 function viewCart() {
